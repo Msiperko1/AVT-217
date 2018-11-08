61f229c4f79b95beba12e4d4d0a69e6fcd69522b
Msiperko1 <42776365+Msiperko1@users.noreply.github.com>
2018-11-08 18:17:42 -0500

    ebeig

---
+++ js/execie12.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    // run function on initial page load
+    interactions();
+    variables();
+ 
+    // run function on resize of the window
+    $(window).resize(function() {
+   
+    });
+ 
+    // run function on scroll
+    $(window).scroll(function() {
+ 
+    });
+});
